e5ad5878f9700f496888f00f9d041fe2d7f35049
VladBorod <jazgigantsmith@gmail.com>
2022-12-12 15:31:50 +0300

    Ex3 Edit

---
+++ Ex1Obj68Akkerman/Ex3Obj68Akkerman.cs
@@ -0,0 +1,22 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+Console.Clear();
+
+Console.WriteLine("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(AkkermanCalc(m, n));
+
+int AkkermanCalc(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return AkkermanCalc(m - 1, 1);
+    else return AkkermanCalc(m - 1, AkkermanCalc(m, n - 1));
+}
