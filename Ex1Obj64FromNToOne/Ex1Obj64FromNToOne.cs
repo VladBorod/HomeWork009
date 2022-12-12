34119d6ccf389d0063fcf3c79dde3f798e4c9283
VladBorod <jazgigantsmith@gmail.com>
2022-12-12 15:31:31 +0300

    Ex1 Edit

---
+++ Ex1Obj64FromNToOne/Ex1Obj64FromNToOne.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. 
+// Напишите программу, которая выведет в
+// се натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string result = Subsequence(N, 1);
+Console.Write(result);
+
+string Subsequence(int start, int end)
+{
+    if (end == start)
+    {
+        return end.ToString();       
+    }
+    return start + " " + Subsequence(start - 1, end);
+}
