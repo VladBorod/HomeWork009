72da172885fc5a397fe6689e9ec9f6a653a8580f
VladBorod <jazgigantsmith@gmail.com>
2022-12-12 15:32:09 +0300

    Ex2 Edit

---
+++ Ex2Obj66SummOfElements/Ex2Obj66SummOfElements.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.Clear();
+
+Console.WriteLine("Введите начальное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конечное число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int temp2 = 0;
+int result = Subsequence(N, M);
+Console.Write(result);
+
+int Subsequence(int start, int end)
+{
+    if (start == end) return start;
+    else if (start < end) return end + Subsequence(start, end - 1);
+    else return end + Subsequence(start, end + 1);
+}
